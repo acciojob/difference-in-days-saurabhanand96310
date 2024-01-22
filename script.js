6ae770d2196e9b22523bcbdb2a61196d083d90f4
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-01-22 18:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	let day1=Date.UTC(dateOne);
+	let day2=Date.UTC(dateTwo);
+	let result=(day2-day1)/8.64 x 10000000;
+	return result;
 };
 
 // Do not change the code below.
